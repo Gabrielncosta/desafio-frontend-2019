80f641960dcf0234e288a79b669333af87440f15
Gabrielncosta <gabrielncosta208@gmail.com>
2020-01-21 20:06:12 -0300

    router

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="../assets/logo.png" alt="">
+                    <p>Produtos</p>
+                    <img src="../assets/shopping-cart.svg" alt="">
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+  export default{
+      name: 'Header'
+  }
+</script>
+
+<style lang="scss" scoped>
+    $primaryColor: #000;
+
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,22 +2,36 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+import produto from '../views/Produto.vue';
+import checkout from '../views/Checkout.vue';
+import carrinho from '../views/Carrinho.vue';
+
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'produto',
+    component: produto
   },
   {
+    path: '/checkout',
+    name: 'checkout',
+    component: checkout,
+  },
+  {
+    path: '/carrinho',
+    name: 'carrinho',
+    component: carrinho,
+  }
+ /*  {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import(/* webpackChunkName: "about" */ /* '../views/About.vue')
+ } */
 ]
 
 const router = new VueRouter({
